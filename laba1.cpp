4501a631cd1224e03a1bfed23b7442bb1a1b3927
Nastya232323 <nastya23232@mail.ru>
2016-06-04 21:36:53 +0400

    Update laba1.cpp

--- laba1.cpp
+++ laba1.cpp
@@ -1,5 +1,3 @@
-
-
 #include "stdafx.h"
 #include <fstream>
 #include <iostream>
@@ -7,36 +5,38 @@
 
 using namespace std;
 
+void proverka(string n1, string n2) {
+	int i = 1;
+	ifstream F1(n1);
+	while (!F1.eof()) {
+		string s1, s2;
+		bool ch = false;
+		ifstream F2(n2);
+		getline(F1, s1);
+		while (!F2.eof()) {
+			getline(F2, s2);
+			if (s1 == s2) {
+				ch = true;
+			}
+		}
+		F2.close();
+		if (ch == false) {
+			cout << n1 << " " << i << " " << s1 << endl;
+			i++;
+		}
+	}
+	F1.close();
+}
 
 int main()
 {
-	int i = 1;
-	int count = 1;
 	string name1;
 	string name2;
 	cin >> name1;
 	cin >> name2;
-
-	ifstream F1(name1);
-	ifstream F2(name2);
-
-	while (!F1.eof() || !F2.eof()) {
-		string s1, s2;
-		getline(F1, s1);
-		getline(F2, s2);
-		if (s1 != s2) {
-			cout << count << " difference"  <<endl;
-			cout << name1 << " " << i << " " << s1 << endl;
-			cout << name2 << " " << i << " " << s2 << endl;
-			cout << endl;
-			count++;
-
-		}
-		i++;
-	}
-
-	F1.close();
-	F2.close();
+	proverka(name1, name2);
+	proverka(name2, name1);
 	system("pause");
-	return 0;
+    return 0;
 }
+
